ce888192c6b12fc0126b8c207316f250eab1f47b
Darlysson Santana <darlysson.alves.dev@gmail.com>
2024-07-29 20:30:14 -0300

    fix: change shadcn to chakraui

--- src/app/_components/video-player.tsx
+++ src/app/_components/video-player.tsx
@@ -1,4 +1,13 @@
-import { useRef, useState, useEffect, useCallback } from "react";
+import {
+  Box,
+  IconButton,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  useColorModeValue,
+  Image as ChakraImage,
+} from "@chakra-ui/react";
 import {
   IoPlay,
   IoPause,
@@ -9,6 +18,7 @@ import {
   IoPlaySkipBack,
   IoPlaySkipForward,
 } from "react-icons/io5";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 interface VideoPlayerProps {
   videoUrl: string;
@@ -16,7 +26,10 @@ interface VideoPlayerProps {
 
 export const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const hiddenVideoRef = useRef<HTMLVideoElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
   const [clickTimeout, setClickTimeout] = useState<NodeJS.Timeout | null>(null);
   const [playing, setPlaying] = useState(false);
   const [muted, setMuted] = useState(false);
@@ -26,6 +39,48 @@ export const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl }) => {
   const [showControls, setShowControls] = useState(true);
   const [mouseMoving, setMouseMoving] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
+  const [previewSrc, setPreviewSrc] = useState("");
+  const [previewTime, setPreviewTime] = useState(0);
+  const [previewX, setPreviewX] = useState(0);
+  const [pageLoaded, setPageLoaded] = useState(false);
+
+  const handleOnMouseMoveTimeline = useCallback(
+    (event: React.MouseEvent<HTMLInputElement>) => {
+      const slider = event.target as HTMLInputElement;
+      const rect = slider.getBoundingClientRect();
+      const offsetX = event.clientX - rect.left;
+      const previewTime = (offsetX / slider.offsetWidth) * duration;
+      setPreviewTime(previewTime);
+      setPreviewX(offsetX);
+      captureFrame(previewTime);
+    },
+    [duration]
+  );
+
+  const handleOnMouseLeaveTimeline = () => {
+    setPreviewTime(0);
+  };
+
+  const captureFrame = (time: number) => {
+    if (hiddenVideoRef.current && canvasRef.current) {
+      const context = canvasRef.current.getContext("2d");
+
+      hiddenVideoRef.current.currentTime = time;
+
+      if (context) {
+        context.drawImage(
+          hiddenVideoRef.current,
+          0,
+          0,
+          canvasRef.current.width,
+          canvasRef.current.height
+        );
+
+        const dataURL = canvasRef.current.toDataURL("image/png");
+        setPreviewSrc(dataURL);
+      }
+    }
+  };
 
   const togglePlay = useCallback(() => {
     if (videoRef.current) {
@@ -47,7 +102,6 @@ export const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl }) => {
 
   const handleTimeUpdate = useCallback(() => {
     if (videoRef.current && !isDragging) {
-      console.log(videoRef.current.duration);
       setCurrentTime(videoRef.current.currentTime);
     }
   }, [isDragging]);
@@ -58,16 +112,12 @@ export const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl }) => {
     }
   }, []);
 
-  const handleSliderChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (videoRef.current) {
-        const newTime = Number(event.target.value);
-        videoRef.current.currentTime = newTime;
-        setCurrentTime(newTime);
-      }
-    },
-    []
-  );
+  const handleSliderChange = useCallback((value: number) => {
+    if (videoRef.current) {
+      videoRef.current.currentTime = value;
+      setCurrentTime(value);
+    }
+  }, []);
 
   const handleSliderMouseDown = useCallback(() => {
     setIsDragging(true);
@@ -176,57 +226,126 @@ export const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl }) => {
     };
   }, [togglePlay]);
 
+  useEffect(() => {
+    setPageLoaded(true);
+  }, []);
+
+  if (!pageLoaded) {
+    return null;
+  }
+
   return (
-    <div
+    <Box
       ref={containerRef}
-      className="relative w-full h-full"
+      position="relative"
+      width="100vw"
+      height="100vh"
       onMouseMove={handleMouseMove}
+      bg="black"
+      overflow="hidden"
     >
+      {!!previewTime && (
+        <ChakraImage
+          src={previewSrc}
+          alt="preview"
+          position="absolute"
+          bottom="24"
+          left={`${previewX}px`}
+          width="200px"
+          height="120px"
+        />
+      )}
       <video
         ref={videoRef}
-        className="w-full h-full object-cover"
+        style={{ width: "100%", height: "100%", objectFit: "cover" }}
         onTimeUpdate={handleTimeUpdate}
-        onPlay={handleLoadedMetadata}
+        onLoadedMetadata={handleLoadedMetadata}
         controls={false}
         disablePictureInPicture
         controlsList="nodownload noremoteplayback nofullscreen"
         onClick={handleClickTogglePlay}
         onDoubleClick={handleDoubleClickToggleScreen}
+        crossOrigin="anonymous"
       >
         <source src={videoUrl} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div
-        className={`controls absolute bottom-0 left-0 right-0 p-4 flex items-center justify-between transition-opacity duration-300 bg-black bg-opacity-75 ${
-          showControls ? "opacity-100" : "opacity-0"
-        }`}
+      <Box
+        position="absolute"
+        bottom="0"
+        left="0"
+        right="0"
+        p="4"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        transition="opacity 0.3s"
+        bg="rgba(0, 0, 0, 0.5)"
+        opacity={showControls ? 1 : 0}
+        zIndex="1"
       >
-        <button onClick={() => handleSkip(-10)} className="text-white">
-          <IoPlaySkipBack size={24} />
-        </button>
-        <button onClick={togglePlay} className="text-white">
-          {playing ? <IoPause size={24} /> : <IoPlay size={24} />}
-        </button>
-        <button onClick={() => handleSkip(10)} className="text-white">
-          <IoPlaySkipForward size={24} />
-        </button>
-        <button onClick={toggleMute} className="text-white">
-          {muted ? <IoVolumeMute size={24} /> : <IoVolumeHigh size={24} />}
-        </button>
-        <input
-          type="range"
-          className="flex-grow mx-4"
+        <IconButton
+          aria-label="Skip back"
+          icon={<IoPlaySkipBack />}
+          onClick={() => handleSkip(-10)}
+          bg="transparent"
+          _hover={{ bg: "transparent" }}
+        />
+        <IconButton
+          aria-label="Play/Pause"
+          icon={playing ? <IoPause /> : <IoPlay />}
+          onClick={togglePlay}
+          bg="transparent"
+          _hover={{ bg: "transparent" }}
+        />
+        <IconButton
+          aria-label="Skip forward"
+          icon={<IoPlaySkipForward />}
+          onClick={() => handleSkip(10)}
+          bg="transparent"
+          _hover={{ bg: "transparent" }}
+        />
+        <IconButton
+          aria-label="Mute/Unmute"
+          icon={muted ? <IoVolumeMute /> : <IoVolumeHigh />}
+          onClick={toggleMute}
+          bg="transparent"
+          _hover={{ bg: "transparent" }}
+        />
+        <Slider
+          focusThumbOnChange={false}
           value={currentTime}
           max={duration}
-          step="0.1"
-          onChange={handleSliderChange}
+          step={0.1}
+          onChange={(val) => handleSliderChange(val)}
           onMouseDown={handleSliderMouseDown}
           onMouseUp={handleSliderMouseUp}
+          flexGrow={1}
+          mx="4"
+        >
+          <SliderTrack>
+            <SliderFilledTrack />
+          </SliderTrack>
+          <SliderThumb />
+        </Slider>
+        <IconButton
+          aria-label="Full screen"
+          icon={isFullScreen ? <IoContract /> : <IoExpand />}
+          onClick={handleFullScreenToggle}
+          bg="transparent"
+          _hover={{ bg: "transparent" }}
         />
-        <button onClick={handleFullScreenToggle} className="text-white">
-          {isFullScreen ? <IoContract size={24} /> : <IoExpand size={24} />}
-        </button>
-      </div>
-    </div>
+      </Box>
+      <canvas ref={canvasRef} style={{ visibility: "hidden" }} />
+      <video
+        ref={hiddenVideoRef}
+        width="640"
+        height="360"
+        style={{ display: "none" }}
+        crossOrigin="anonymous"
+      >
+        <source src={videoUrl} type="video/mp4" />
+      </video>
+    </Box>
   );
 };
